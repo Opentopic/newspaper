ad6279a880d75a3c7ebc8eec97bb58670308a4d4
kisiel <kisiel85@go2.pl>
2016-02-11 09:17:06 +0100

    casperjs - disable throwing errors

--- newspaper/casperjs/get_page_content.js
+++ newspaper/casperjs/get_page_content.js
@@ -5,4 +5,11 @@ var url = casper.cli.get(0);
 casper.start(url, function() {
     this.echo(this.getHTML());
 });
-casper.run();
+casper.run(function() {
+    var _this = this;
+    _this.page.close();
+    setTimeout(function(){ phantom.exit(0); }, 0);
+    phantom.onError = function(){};
+    throw new Error('');
+});
+//casper.run();
