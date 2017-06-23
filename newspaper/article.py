72920fe0077ba3258d3a1f408bede2cd111489da
Jan Waś <janwas@tjsoftware.co>
2017-06-23 13:07:55 +0200

    change stopword lang detection to an external library

--- newspaper/article.py
+++ newspaper/article.py
@@ -10,11 +10,7 @@ import os
 import glob
 from bs4 import BeautifulSoup
 
-try:
-    from nltk import wordpunct_tokenize
-    from nltk.corpus import stopwords, swadesh
-except ImportError:
-    print('[!] You need to install nltk (http://nltk.org/index.html)')
+from langdetect import detect as langdetect
 
 from . import images
 from . import network
@@ -276,9 +272,6 @@ class Article(object):
         Calculate probability of given text to be written in several languages and
         return the highest scored.
 
-        It uses a stopwords based approach, counting how many unique stopwords
-        are seen in analyzed text.
-
         :param text: Text whose language want to be detected
         :type text: str
 
@@ -294,43 +287,7 @@ class Article(object):
             text = self.text
 
         text = BeautifulSoup(text, "html.parser").get_text()
-        ratios = self._calculate_languages_ratios(text)
-
-        if not ratios:
-            return
-
-        most_rated_language = max(ratios, key=ratios.get)
-
-        self.set_language(most_rated_language)
-
-    def _calculate_languages_ratios(self, text):
-        """
-        Calculate probability of given text to be written in several languages and
-        return a dictionary that looks like {'french': 2, 'spanish': 4, 'english': 0}
-
-        :param text: Text whose language want to be detected
-        :type text: str
-
-        :return: Dictionary with languages and unique stopwords seen in analyzed text
-        :rtype: dict
-        """
-
-        languages_ratios = {}
-
-        tokens = wordpunct_tokenize(text)
-        words = [word.lower() for word in tokens]
-
-        # Compute per language included in nltk number of unique stopwords appearing in analyzed text
-        for language in stopwords.fileids():
-            stopwords_set = set(stopwords.words(language))
-            words_set = set(words)
-            common_elements = words_set.intersection(stopwords_set)
-            if len(common_elements) <= 2:
-                continue
-
-            languages_ratios[language] = len(common_elements)  # language "score"
-
-        return languages_ratios
+        self.set_language(langdetect(text))
 
     def fetch_images(self, fetch_hash=False):
         if self.clean_doc is not None:
